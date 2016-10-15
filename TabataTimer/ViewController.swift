989b4a16a687b2bfadb01f52f0c666b9bcc720e9
ttorii20 <toriitakahiro@toriitakahiro-no-macbook-air.local>
2016-10-15 18:23:31 +0900

    Initial Commit

---
+++ TabataTimer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TabataTimer
+//
+//  Created by 鳥居隆弘 on 2016/10/15.
+//  Copyright © 2016年 鳥居隆弘. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
